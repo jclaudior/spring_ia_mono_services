bdd2891aa95d39d33782dc2513402b2f16f20051
jclaudior <joaoclaudiodiva@gmail.com>
2026-03-15 19:06:18 -0300

    add structure

--- src/main/java/com/jcr/monoservices/adapter/in/web/ApplicationController.java
+++ src/main/java/com/jcr/monoservices/adapter/in/web/ApplicationController.java
@@ -1,4 +1,15 @@
 package com.jcr.monoservices.adapter.in.web;
 
+import com.jcr.monoservices.application.port.in.ApplicationUseCase;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/applications")
 public class ApplicationController {
+    @Autowired
+    private ApplicationUseCase createUseCase;
+
+
 }
--- src/main/java/com/jcr/monoservices/application/service/ApplicationService.java
+++ src/main/java/com/jcr/monoservices/application/service/ApplicationService.java
@@ -1,6 +1,8 @@
 package com.jcr.monoservices.application.service;
 
 import com.jcr.monoservices.application.port.in.ApplicationUseCase;
+import org.springframework.stereotype.Service;
 
+@Service
 public class ApplicationService  implements ApplicationUseCase {
 }
